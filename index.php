48322f63c4b498810f30c468be080748ae9cf4ab
arqui1-2018 <arqui1_2018@outlook.es>
2018-09-16 12:58:50 -0600

    actual api

--- index.php
+++ index.php
@@ -1,34 +1,65 @@
 <?php
 
+date_default_timezone_set('Central');
 require_once __DIR__ . '/public/functions.php';
 
 router('GET', '^/$', function() {
+    echo 'Esta es una api, hola!!';
+});
+
+router('GET', '^/query$', function() {
     $manager = new MongoDB\Driver\Manager("mongodb://arqui1db-2018:jY4INnddURHmEEJDL05qGHEYGZVQgGvf4EmomytTCqhf3wsxhuxbUPN9CJAzkJWyKvt9MLrfx1TOdxOwhNT1Xw==@arqui1db-2018.documents.azure.com:10255/?ssl=true&replicaSet=globaldb");
-    $query = new MongoDB\Driver\Query(array('id'=>'201513744'));
-    $cursor = $manager->executeQuery('globaldb.posts', $query);
-    foreach ($cursor as $document) {
-        echo "{ Name: $document->Name , id: $document->id }";
-    }
+    $query = new MongoDB\Driver\Query(array());
+    $cursor = $manager->executeQuery('admin.Entrada', $query);
+    $array = $cursor->toArray();
+    $data = "";
+    $data = '[{"Fecha":"'.$array[0]->Fecha.'", "Mensaje":"'.$array[0]->Mensaje.'"}';
+    for($i = 1; $i < count($array); $i++)
+         $data.=',{"Fecha":"'.$array[$i]->Fecha.'", "Mensaje":"'.$array[$i]->Mensaje.'"}';    
+    $data .= "]";
+    echo $data;
+    // foreach ($cursor as $document) {
+    //     echo "{ Fecha: $document->Fecha , Mensaje: $document->Mensaje }";
+    // }
 });
 
-// GET request to /users
-router('GET', '^/users$', function() {
-    echo '<a href="users/1000">Show user: 1000</a>';
+// // With named parameters
+// router('GET', '^/users/(?<id>\d+)$', function($params) {
+//     echo "You selected User-ID: ";
+//     var_dump($params);
+// });
+
+// // POST request to /users
+// router('POST', '^/users$', function() {
+//     header('Content-Type: application/json');
+//     $json = json_decode(file_get_contents('php://input'), true);
+//     echo json_encode(['result' => 1]);
+// });
+
+// GET insert Correct
+router('GET', '^/insert/correct$', function(){
+    $bulk = new MongoDB\Driver\BulkWrite();
+
+    $bulk->insert(['Fecha' => date(DATE_RFC2822), 'Mensaje' => 'Correcto']);
+
+    $manager = new MongoDB\Driver\Manager("mongodb://arqui1db-2018:jY4INnddURHmEEJDL05qGHEYGZVQgGvf4EmomytTCqhf3wsxhuxbUPN9CJAzkJWyKvt9MLrfx1TOdxOwhNT1Xw==@arqui1db-2018.documents.azure.com:10255/?ssl=true&replicaSet=globaldb");
+    $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 100);
+    $result = $manager->executeBulkWrite('admin.Entrada', $bulk, $writeConcern);
+
 });
 
-// With named parameters
-router('GET', '^/users/(?<id>\d+)$', function($params) {
-    echo "You selected User-ID: ";
-    var_dump($params);
-});
+// GET insert Incorrect
+router('GET', '^/insert/incorrect$', function(){
+    $bulk = new MongoDB\Driver\BulkWrite();
 
-// POST request to /users
-router('POST', '^/users$', function() {
-    header('Content-Type: application/json');
-    $json = json_decode(file_get_contents('php://input'), true);
-    echo json_encode(['result' => 1]);
-});
+    $bulk->insert(['Fecha' => date(DATE_RFC2822), 'Mensaje' => 'Incorrecto']);
 
+    $manager = new MongoDB\Driver\Manager("mongodb://arqui1db-2018:jY4INnddURHmEEJDL05qGHEYGZVQgGvf4EmomytTCqhf3wsxhuxbUPN9CJAzkJWyKvt9MLrfx1TOdxOwhNT1Xw==@arqui1db-2018.documents.azure.com:10255/?ssl=true&replicaSet=globaldb");
+    $writeConcern = new MongoDB\Driver\WriteConcern(MongoDB\Driver\WriteConcern::MAJORITY, 100);
+    $result = $manager->executeBulkWrite('admin.Entrada', $bulk, $writeConcern);
+
+});
 header("HTTP/1.0 404 Not Found");
 echo '404 Not Found';
+
 ?>
